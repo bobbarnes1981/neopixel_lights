5f3d617e76adc8b779ae10022d980ce5a053b640
Robert Barnes <robertbarnes1981@googlemail.com>
2021-10-12 14:53:09 +0100

    Create api to adjust settings

--- neopixel_lights/website.py
+++ neopixel_lights/website.py
@@ -1,3 +1,4 @@
+import json
 import os
 import web
 
@@ -10,7 +11,9 @@ lights = Lights()
 class Website(object):
     def __init__(self):
         urls = (
-            '/', 'index'
+            '/', 'index',
+            '/api/colours', 'api_colours',
+            '/api/brightness', 'api_brightness',
         )
         self.app = web.application(urls, globals())
     def start(self):
@@ -20,4 +23,18 @@ class Website(object):
 class index(object):
     def GET(self):
         return render.index(lights)
+
+class api_colours(object):
+    def GET(self):
+        return json.dumps({'colours': lights.colours})
+
+class api_brightness(object):
+    def GET(self):
+        return json.dumps({'brightness': lights.brightness})
+    def PUT(self):
+        data = json.loads(web.data())
+        # TODO: validate brightness 0.0 - 1.0
+        lights.brightness = float(data['brightness'])
+        lights.create()
+        return json.dumps({'brightness': lights.brightness})
 
